988f33474822d4b715b583493b5735119ac077bd
joao.neves <joao95neves@gmail.com>
2024-11-15 11:10:48 +0000

    feat(lists): add more utils

--- src/lists.py
+++ src/lists.py
@@ -8,6 +8,18 @@ def try_get(target: list[Any], index: int, default: Any) -> Any:
         return default
 
 
+def strip_str_list(source: list[str]):
+    return [item for item in source if item.strip()]
+
+
+def remove_in_str_list(source: list[str], sub_str: str):
+    return replace_in_str_list(source, sub_str, "")
+
+
+def replace_in_str_list(source: list[str], sub_str: str, replacer: str):
+    return [item.replace(sub_str, replacer) for item in source]
+
+
 def table_to_string(table: list[list[str]], column_separator: str) -> str:
     """
     Converts a matrix table into a string, where each row is separated by "\\n"
