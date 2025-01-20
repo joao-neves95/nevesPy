a042a3fe7595e68239a7047ed361d7a1305b386c
joao.neves <joao95neves@gmail.com>
2025-01-20 17:37:57 +0000

    feat(fs/csv): append ".csv" in case the input doesn't end with it

--- src/fs/csv.py
+++ src/fs/csv.py
@@ -3,6 +3,13 @@ from typing import Any, Iterable
 
 
 def write_csv_file(path: str, data: Iterable[Iterable[Any]]):
+    """
+    Writes a CSV file with the given data.
+    If the path does not end with ".csv", it will be appended.
+    """
+    if path.endswith(".csv") is False:
+        path = f"{path}.csv"
+
     with open(path, mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerows(data)
